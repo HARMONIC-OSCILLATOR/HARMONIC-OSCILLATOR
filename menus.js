8b5a223dbce18f7a2d5225051c7b558fe428f8c0
MNO <osc.symbol@gmail.com>
2022-02-03 20:45:55 -0800

    html tryharding

---
+++ menus.js
@@ -0,0 +1,24 @@
+//MENU HTML & SVG STORAGE SCRIPT
+//MNO
+
+var left-nav = document.getElementById('left');
+var right-nav = document.getElementById('right');
+var osc-sticker-1 = document.getElementById('');
+var osc-sticker-2 = document.getElementById('');
+var osc-sticker-3 = document.getElementById('');
+var osc-sticker-4 = document.getElementById('');
+var osc-sticker-1-wide = document.getElementById('');
+var osc-sticker-2-wide = document.getElementById('');
+var osc-sticker-3-wide = document.getElementById('');
+var osc-sticker-4-wide = document.getElementById('');
+
+var left-nav-codeblock = ;
+var right-nav-codeblock = ;
+var osc-sticker-1-codeblock = ;
+var osc-sticker-2-codeblock = ;
+var osc-sticker-3-codeblock = ;
+var osc-sticker-4-codeblock = ;
+var osc-sticker-1-wide-codeblock = ;
+var osc-sticker-2-wide-codeblock = ;
+var osc-sticker-3-wide-codeblock = ;
+var osc-sticker-4-wide-codeblock = ;
