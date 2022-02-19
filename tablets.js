094df8d3e0df036111d2fa75741550b761f0739e
MNO <osc.symbol@gmail.com>
2022-02-18 20:09:44 -0800

    TABLET RANDOMIZER!

--- tablets.js
+++ tablets.js
@@ -1,19 +1,30 @@
 //HARMONIC OSCILLATOR COUNTERGLOW TABLET MANAGER
 //MNO
 
+var num_tablet_types = 0;
+
+class tablet_type {
+	constructor (text, tag){
+		this.text = text;
+		this.tag = tag;
+		this.id = num_tablet_types++;
+	}
+}
+
+
 var tablets = document.getElementsByClassName('tablet-of-truth');
-//var tablet_1 = document.getElementById('tablet-1');
-//var tablet_2 = document.getElementById('tablet-2');
-//var tablet_3 = document.getElementById('tablet-3');
 
-var tablet_text_standalone_1 = '<p>Furthermore, the object, which individuality has as its essence, has no content. It is what is called necessity, since necessity, fate, and the like, each amount to saying that we do not know how to say what it is doing or what its determinate laws and its positive content are supposed to be because it is the absolute pure concept itself intuited as being, the simple and empty but nonetheless inexorable and impassive relation whose work is merely the nothingness of individuality.</p>';
-var tablet_text_standalone_2 = '<p>Furthermore, since the skull does not itself feel, it seems that perhaps a more determinate significance could be given to it. Through their proximity to the skull, certain determinate sensations would allow us to take cognizance83 of what the skull is supposed to mean, and if a conscious mode of spirit has its feeling in a determinate place on the skull, then perhaps this place on the skull will indicate by its shape that mode of spirit and its particularity.</p>';
+//ADDING NEW TABLETS
+//FORMAT AS new tablet_type('text','tag'); where tag is either 'none', 'tall', or 'wide'
 
-var tablet_text_with_tallglyph_1 = '<p>Spirit is the self of the actual consciousness which spirit confronts, or rather which confronts itself as an objective actual world, a world which has likewise lost all its significance as something alien for the self, just as the self has lost any sense of being a dependent or independent being-for-itself separated from that world.</p>';
-var tablet_text_with_tallglyph_2 = '<p>The being of what was meant, along with the individuality and the universality opposed to that individuality in perception, as well as the empty inner of the understanding, no longer exist as the essence. Rather, they exist merely as moments of self-consciousness.</p>';
+var tablet_text_standalone_1 = new tablet_type('<p>Furthermore, the object, which individuality has as its essence, has no content. It is what is called necessity, since necessity, fate, and the like, each amount to saying that we do not know how to say what it is doing or what its determinate laws and its positive content are supposed to be because it is the absolute pure concept itself intuited as being, the simple and empty but nonetheless inexorable and impassive relation whose work is merely the nothingness of individuality.</p>', 'none');
+var tablet_text_standalone_2 = new tablet_type('<p>Furthermore, since the skull does not itself feel, it seems that perhaps a more determinate significance could be given to it. Through their proximity to the skull, certain determinate sensations would allow us to take cognizance83 of what the skull is supposed to mean, and if a conscious mode of spirit has its feeling in a determinate place on the skull, then perhaps this place on the skull will indicate by its shape that mode of spirit and its particularity.</p>', 'none');
 
-var tablet_text_with_wideglyph_1 = '<p>This speech is the madness of the musician “who piled up and mixed together some thirty airs, Italian, French, tragic, comic, of all sorts of character; now, with a deep bass, he descended into the depths of hell, then, contracting his throat, with a falsetto he tore apart the vaults of the skies, alternately raging and then being placated, imperious and then derisive.”</p>';
-var tablet_text_with_wideglyph_2 = '<p>The unity is estranged because it is absolutely negative unity, that is, infinite unity, and because the unity is durable existence, the distinction also has self-sufficiency only in the unity. This self-sufficiency of the shape appears as something determinate, as for an other, for it is something estranged. The sublation of the estrangement likewise happens by way of an other.</p>';
+var tablet_text_with_tallglyph_1 = new tablet_type('<p>Spirit is the self of the actual consciousness which spirit confronts, or rather which confronts itself as an objective actual world, a world which has likewise lost all its significance as something alien for the self, just as the self has lost any sense of being a dependent or independent being-for-itself separated from that world.</p>', 'tall');
+var tablet_text_with_tallglyph_2 = new tablet_type('<p>The being of what was meant, along with the individuality and the universality opposed to that individuality in perception, as well as the empty inner of the understanding, no longer exist as the essence. Rather, they exist merely as moments of self-consciousness.</p>', 'tall');
+
+var tablet_text_with_wideglyph_1 = new tablet_type('<p>This speech is the madness of the musician “who piled up and mixed together some thirty airs, Italian, French, tragic, comic, of all sorts of character; now, with a deep bass, he descended into the depths of hell, then, contracting his throat, with a falsetto he tore apart the vaults of the skies, alternately raging and then being placated, imperious and then derisive.”</p>', 'wide');
+var tablet_text_with_wideglyph_2 = new tablet_type('<p>The unity is estranged because it is absolutely negative unity, that is, infinite unity, and because the unity is durable existence, the distinction also has self-sufficiency only in the unity. This self-sufficiency of the shape appears as something determinate, as for an other, for it is something estranged. The sublation of the estrangement likewise happens by way of an other.</p>', 'wide');
 
 var tall_glyph_1 =
 		  '<div class="glyph glyph-tall">' +
@@ -75,12 +86,27 @@ var wide_glyph_2 =
 			  '</svg>' +
 			'</div>';
 
-var tablet_texts[tablet_text_standalone_1, tablet_text_standalone_2, tablet_text_with_tallglyph_1, tablet_text_with_tallglyph_2, tablet_text_with_wideglyph_1, tablet_text_with_wideglyph_2];
-var tall_glyphs[tall_glyph_1, tall_glyph_2];
-var wide_glyphs[wide_glyph_1, wide_glyph_2];
+var tablet_texts = [tablet_text_standalone_1, tablet_text_standalone_2, tablet_text_with_tallglyph_1, tablet_text_with_tallglyph_2, tablet_text_with_wideglyph_1, tablet_text_with_wideglyph_2];
+var tall_glyphs = [tall_glyph_1, tall_glyph_2];
+var wide_glyphs = [wide_glyph_1, wide_glyph_2];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max); //.floor is good for arrays since they start at 0!
+}
 
 function populateTablets() {
+	console.log("THIS WORKS!!!!!!!!!!!!!");
 	for(i = 0; i < tablets.length; i++) {
+		let type = tablet_texts[getRandomInt(tablet_texts.length)];
+		let glyph = '';
 		
+		if (type.tag == 'tall') {
+			glyph = tall_glyphs[getRandomInt(tall_glyphs.length)];
+		}
+		else if (type.tag == 'wide') {
+			glyph = wide_glyphs[getRandomInt(wide_glyphs.length)];
+		}
+		
+		tablets[i].innerHTML = type.text + glyph;
 	}
 }
