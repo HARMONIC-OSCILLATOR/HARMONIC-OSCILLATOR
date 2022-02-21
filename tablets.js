ce94a41622186417f94d6c6f4a5f1101f410d305
MNO <osc.symbol@gmail.com>
2022-02-20 16:18:06 -0800

    scissors svg

--- tablets.js
+++ tablets.js
@@ -26,7 +26,24 @@ var tablet_text_with_tallglyph_2 = new tablet_type('<p>The being of what was mea
 var tablet_text_with_wideglyph_1 = new tablet_type('<p>This speech is the madness of the musician “who piled up and mixed together some thirty airs, Italian, French, tragic, comic, of all sorts of character; now, with a deep bass, he descended into the depths of hell, then, contracting his throat, with a falsetto he tore apart the vaults of the skies, alternately raging and then being placated, imperious and then derisive.”</p>', 'wide');
 var tablet_text_with_wideglyph_2 = new tablet_type('<p>The unity is estranged because it is absolutely negative unity, that is, infinite unity, and because the unity is durable existence, the distinction also has self-sufficiency only in the unity. This self-sufficiency of the shape appears as something determinate, as for an other, for it is something estranged. The sublation of the estrangement likewise happens by way of an other.</p>', 'wide');
 
-var tall_glyph_1 =
+var tall_glyph_1 = //SCISSOR TALL
+		  '<div class="glyph glyph-tall">' +
+		    '<svg class="glyph-svg-original" width="60" height="120">' +
+		      '<polygon points="" fill="none" stroke="#AA3090" stroke-width="1">' +
+			  '<animate attributeName="points" values="32,100 22,60 12,50 12,20 28,20 28,46 35,60; 45,97 24,62 11,56 2,27 17,22 24,47 36,58; 32,100 22,60 12,50 12,20 28,20 28,46 35,60;" dur="1s" repeatCount="indefinite">' +
+			  '</polygon>' +
+			  '<polygon points="" fill="none" stroke="#AA3090" stroke-width="1">' +
+			  '<animate attributeName="points" values="32,100 22,60 32,30 48,30 48,45 36,52; 13,97 22,56 45,35 58,41 51,55 38,56; 32,100 22,60 32,30 48,30 48,45 36,52;" dur="1s" repeatCount="indefinite">' +
+			  '</polygon>' +
+		    '</svg>' +
+			'<svg class="glyph-svg-small" width="50" height="100">' +
+		      '<polygon points="" fill="none" stroke="#AA3090" stroke-width="1">' +
+			  '<animate attributeName="points" values="" dur="1s" repeatCount="indefinite">' +
+			  '</polygon>' +
+		    '</svg>' +
+		  '</div>';
+
+var tall_glyph_2 = //TEMP DIAMOND
 		  '<div class="glyph glyph-tall">' +
 		    '<svg class="glyph-svg-original" width="60" height="120">' +
 		      '<polygon points="30,5 55,60 30,115 5,60" fill="none" stroke="#AA3090" stroke-width="1"/>' +
@@ -36,17 +53,7 @@ var tall_glyph_1 =
 		    '</svg>' +
 		  '</div>';
 
-var tall_glyph_2 =
-		  '<div class="glyph glyph-tall">' +
-		    '<svg class="glyph-svg-original" width="60" height="120">' +
-		      '<polygon points="30,2 55,62 30,125 5,62" fill="none" stroke="#AA3090" stroke-width="1"/>' +
-		    '</svg>' +
-			'<svg class="glyph-svg-small" width="50" height="100">' +
-		      '<polygon points="25,2 45,52 25,25 5,50" fill="none" stroke="#AA3090" stroke-width="1"/>' +
-		    '</svg>' +
-		  '</div>';
-
-var wide_glyph_1 =
+var wide_glyph_1 = //SCISSOR WIDE
 		    '<div class="glyph glyph-wide">' +	  
 			  '<svg class="glyph-svg-original" width="100" height="50">' +
 			    '<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
@@ -54,7 +61,7 @@ var wide_glyph_1 =
                 '</path>' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
                 '<animate attributeName="d" values="M75,20 L45,20 L35,10 L25,20 L45,30 L75,20; M71,40 L49,21 L47,7 L33,9 L42,30 L71,40; M75,20 L45,20 L35,10 L25,20 L45,30 L75,20;" dur="1s" repeatCount="indefinite">' +
-                '</polygon>' +
+                '</path>' +
 			  '</svg>' +
 			  '<svg class="glyph-svg-small" width="60" height="50">' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
@@ -62,27 +69,27 @@ var wide_glyph_1 =
                 '</path>' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
                 '<animate attributeName="d" values="M55,20 L25,20 L15,10 L5,20 L25,30 L55,20; M51,40 L29,21 L27,7 L13,9 L22,30 L51,40; M55,20 L25,20 L15,10 L5,20 L25,30 L55,20;" dur="1s" repeatCount="indefinite">' +
-                '</polygon>' +
+                '</path>' +
 			  '</svg>' +
 			'</div>';
 
-var wide_glyph_2 =
+var wide_glyph_2 = //EMPTY
 		    '<div class="glyph glyph-wide">' +	  
 			  '<svg class="glyph-svg-original" width="100" height="50">' +
 			    '<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="M75,22 L45,22 L40,22 L25,22 L25,35 L45,35 L50,25 L75,20; M73,12 L43,22 L40,22 L26,30 L30,40 L48,35 L50,22 L73,12; M75,20 L45,20 L40,25 L25,25 L25,35 L45,35 L50,25 L75,20;" dur="1s" repeatCount="indefinite">' +
+                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite">' +
                 '</path>' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="M75,20 L45,20 L35,10 L25,22 L42,32 L75,22; M71,20 L42,21 L42,7 L33,9 L42,30 L71,20; M75,20 L45,20 L35,10 L25,20 L45,30 L75,20;" dur="1s" repeatCount="indefinite">' +
-                '</polygon>' +
+                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite">' +
+                '</path>' +
 			  '</svg>' +
 			  '<svg class="glyph-svg-small" width="60" height="50">' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="M55,20 L25,20 L20,25 L5,25 L5,35 L25,35 L30,25 L55,20; M53,12 L23,20 L20,26 L6,30 L10,40 L28,35 L30,22 L53,12; M55,20 L25,20 L20,25 L5,25 L5,35 L25,35 L30,25 L55,20;" dur="1s" repeatCount="indefinite">' +
+                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite">' +
                 '</path>' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="M55,20 L25,20 L15,10 L5,20 L25,30 L55,20; M51,40 L29,21 L27,7 L13,9 L22,30 L51,40; M55,20 L25,20 L15,10 L5,20 L25,30 L55,20;" dur="1s" repeatCount="indefinite">' +
-                '</polygon>' +
+                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite">' +
+                '</path>' +
 			  '</svg>' +
 			'</div>';
 
