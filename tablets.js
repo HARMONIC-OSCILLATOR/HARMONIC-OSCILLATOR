5e64203b061a23c7864e59ac637311a045b0a9f1
MNO <osc.symbol@gmail.com>
2022-02-28 17:07:03 -0800

    screw svg

--- tablets.js
+++ tablets.js
@@ -14,8 +14,11 @@ class tablet_type {
 
 var tablets = document.getElementsByClassName('tablet-of-truth');
 
-//ADDING NEW TABLETS
-//FORMAT AS new tablet_type('text','tag'); where tag is either 'none', 'tall', or 'wide'
+//ADDING NEW TABLETS?
+//FORMAT AS:
+//new tablet_type('<p>text</p>','tag');
+//where tag is either 'none', 'tall', or 'wide'
+//none will give you no glyph, tall gives a tall glyph, wide gives a wide glyph!
 
 var tablet_text_standalone_1 = new tablet_type('<p>Furthermore, the object, which individuality has as its essence, has no content. It is what is called necessity, since necessity, fate, and the like, each amount to saying that we do not know how to say what it is doing or what its determinate laws and its positive content are supposed to be because it is the absolute pure concept itself intuited as being, the simple and empty but nonetheless inexorable and impassive relation whose work is merely the nothingness of individuality.</p>', 'none');
 var tablet_text_standalone_2 = new tablet_type('<p>Furthermore, since the skull does not itself feel, it seems that perhaps a more determinate significance could be given to it. Through their proximity to the skull, certain determinate sensations would allow us to take cognizance of what the skull is supposed to mean, and if a conscious mode of spirit has its feeling in a determinate place on the skull, then perhaps this place on the skull will indicate by its shape that mode of spirit and its particularity.</p>', 'none');
@@ -50,24 +53,24 @@ var tall_glyph_2 = //SCREW TALL
 		  '<div class="glyph glyph-tall">' +
 		    '<svg class="glyph-svg-original" width="60" height="120">' +
 			  '<ellipse cx="30" cy="80" rx="1" ry="0.625" fill="none" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="80; 70;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="1; 1.875;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="0.625; 1.25;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="80; 70;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="1; 1.875;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="0.625; 1.25;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 			  '<ellipse cx="30" cy="70" rx="1.875" ry="1.25" fill="none" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="70; 60;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="1.875; 3.75;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="1.25; 2.5;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="70; 60;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="1.875; 3.75;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="1.25; 2.5;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 			  '<ellipse cx="30" cy="60" rx="3.75" ry="2.5" fill="none" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="60; 50;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="3.75; 7.5;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="2.5; 5;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="60; 50;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="3.75; 7.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="2.5; 5;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 			  '<ellipse cx="30" cy="50" rx="7.5" ry="5" fill="#100510" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="50; 40;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="7.5; 15;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="5; 10;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="50; 40;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="7.5; 15;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="5; 10;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 		      '<ellipse cx="30" cy="40" rx="15" ry="10" fill="#100510" stroke="#AA3090" stroke-width="1"/>' +
 			  '<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
@@ -76,24 +79,24 @@ var tall_glyph_2 = //SCREW TALL
 		    '</svg>' +
 			'<svg class="glyph-svg-small" width="50" height="100">' +
 			  '<ellipse cx="25" cy="80" rx="1" ry="0.625" fill="none" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="80; 70;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="1; 1.875;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="0.625; 1.25;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="80; 70;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="1; 1.875;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="0.625; 1.25;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 			  '<ellipse cx="25" cy="70" rx="1.875" ry="1.25" fill="none" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="70; 60;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="1.875; 3.75;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="1.25; 2.5;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="70; 60;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="1.875; 3.75;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="1.25; 2.5;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 			  '<ellipse cx="25" cy="60" rx="3.75" ry="2.5" fill="none" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="60; 50;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="3.75; 7.5;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="2.5; 5;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="60; 50;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="3.75; 7.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="2.5; 5;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 			  '<ellipse cx="25" cy="50" rx="7.5" ry="5" fill="#100510" stroke="#AA3090" stroke-width="1">' +
-			  '<animate attributeName="cy" values="50; 40;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="rx" values="7.5; 15;" dur="1s" repeatCount="indefinite"/>' +
-			  '<animate attributeName="ry" values="5; 10;" dur="1s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="cy" values="50; 40;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="rx" values="7.5; 15;" dur="0.5s" repeatCount="indefinite"/>' +
+			  '<animate attributeName="ry" values="5; 10;" dur="0.5s" repeatCount="indefinite"/>' +
 			  '</ellipse>' +
 		      '<ellipse cx="25" cy="40" rx="15" ry="10" fill="#100510" stroke="#AA3090" stroke-width="1"/>' +
 			  '<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
@@ -102,13 +105,23 @@ var tall_glyph_2 = //SCREW TALL
 		    '</svg>' +
 		  '</div>';
 
-var tall_glyph_3 = //TEMP DIAMOND
+var tall_glyph_3 = //TEMP DIAMOND TALL
 		  '<div class="glyph glyph-tall">' +
 		    '<svg class="glyph-svg-original" width="60" height="120">' +
 			  '<polygon points="30,5 55,60 30,115 5,60" fill="none" stroke="#AA3090" stroke-width="1"/>' +
 			'</svg>' +
 			'<svg class="glyph-svg-small" width="50" height="100">' +
 		      '<polygon points="25,5 45,50 25,95 5,50" fill="none" stroke="#AA3090" stroke-width="1"/>' +
+		    '</svg>' +
+		  '</div>';
+
+var tall_glyph_4 = //EMPTY
+		  '<div class="glyph glyph-tall">' +
+		    '<svg class="glyph-svg-original" width="60" height="120">' +
+			  '<polygon points="" fill="none" stroke="#AA3090" stroke-width="1"/>' +
+			'</svg>' +
+			'<svg class="glyph-svg-small" width="50" height="100">' +
+		      '<polygon points="" fill="none" stroke="#AA3090" stroke-width="1"/>' +
 		    '</svg>' +
 		  '</div>';
 
@@ -132,29 +145,85 @@ var wide_glyph_1 = //SCISSOR WIDE
 			  '</svg>' +
 			'</div>';
 
-var wide_glyph_2 = //EMPTY
+var wide_glyph_2 = //SCREW WIDE (IN PROGRESS)
 		    '<div class="glyph glyph-wide">' +	  
 			  '<svg class="glyph-svg-original" width="100" height="50">' +
-			    '<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite"/>' +
-                '</path>' +
+			    '<ellipse cx="70" cy="25" rx="0.625" ry="1" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="60; 70;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="1.25; 0.625;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="1.875; 1;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="60" cy="25" rx="1.25" ry="1.875" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="50; 60;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="2.5; 1.25;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="3.75; 1.875;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="50" cy="25" rx="2.5" ry="3.75" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="40; 50;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="5; 2.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="7.5; 3.75;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="40" cy="25" rx="5" ry="7.5" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="30; 40;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="10; 5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="15; 7.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+			    '<ellipse cx="30" cy="25" rx="10" ry="15" fill="#100510" stroke="#AA3090" stroke-width="1"/>' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite"/>' +
-                '</path>' +
+				'<animate attributeName="d" values="M25,25 L35,25 M30,18 L30,32; M30,18 L30,32 M35,25 L25,25; M35,25 L25,25 M30,32 L30,18;" dur="1s" repeatCount="indefinite"/>' +
+				'</path>' +
 			  '</svg>' +
 			  '<svg class="glyph-svg-small" width="60" height="50">' +
+			    '<ellipse cx="60" cy="25" rx="0.625" ry="1" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="52.5; 60;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="1.25; 0.625;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="1.875; 1;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="52.5" cy="25" rx="1.25" ry="1.875" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="45; 52.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="2.5; 1.25;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="3.75; 1.875;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="45" cy="25" rx="2.5" ry="3.75" fill="none" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="37.5; 45;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="5; 2.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="7.5; 3.75;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="37.5" cy="25" rx="5" ry="7.5" fill="#100510" stroke="#AA3090" stroke-width="1">' +
+			    '<animate attributeName="cx" values="30; 37.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="rx" values="10; 5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '<animate attributeName="ry" values="15; 7.5;" dur="0.5s" repeatCount="indefinite"/>' +
+			    '</ellipse>' +
+				'<ellipse cx="30" cy="25" rx="10" ry="15" fill="#100510" stroke="#AA3090" stroke-width="1"/>' +
 				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite"/>' +
-                '</path>' +
-				'<path d="" fill="none" stroke="#AA3090" stroke-width="1">' +
-                '<animate attributeName="d" values="" dur="1s" repeatCount="indefinite"/>' +
-                '</path>' +
+				'<animate attributeName="d" values="M25,25 L35,25 M30,18 L30,32; M30,18 L30,32 M35,25 L25,25; M35,25 L25,25 M30,32 L30,18;" dur="1s" repeatCount="indefinite"/>' +
+				'</path>' +
+			  '</svg>' +
+			'</div>';
+
+var wide_glyph_3 = //TEMP DIAMOND WIDE
+		    '<div class="glyph glyph-wide">' +	  
+			  '<svg class="glyph-svg-original" width="100" height="50">' +
+			    '<polygon points="15,25 50,5 85,25 50,45" fill="none" stroke="#AA3090" stroke-width="1"/>' +
+			  '</svg>' +
+			  '<svg class="glyph-svg-small" width="60" height="50">' +
+				'<polygon points="4,25 30,9 56,25 30,41" fill="none" stroke="#AA3090" stroke-width="1"/>' +
+			  '</svg>' +
+			'</div>';
+
+var wide_glyph_4 = //EMPTY
+		    '<div class="glyph glyph-wide">' +	  
+			  '<svg class="glyph-svg-original" width="100" height="50">' +
+			    '<polygon points="" fill="none" stroke="#AA3090" stroke-width="1"/>' +
+			  '</svg>' +
+			  '<svg class="glyph-svg-small" width="60" height="50">' +
+				'<polygon points="" fill="none" stroke="#AA3090" stroke-width="1"/>' +
 			  '</svg>' +
 			'</div>';
 
 var tablet_texts = [tablet_text_standalone_1, tablet_text_standalone_2, tablet_text_with_tallglyph_1, tablet_text_with_tallglyph_2, tablet_text_with_wideglyph_1, tablet_text_with_wideglyph_2];
-var tall_glyphs = [tall_glyph_1, tall_glyph_2, tall_glyph_3];
-var wide_glyphs = [wide_glyph_1, wide_glyph_2];
+var tall_glyphs = [tall_glyph_1, tall_glyph_2, tall_glyph_3, tall_glyph_4];
+var wide_glyphs = [wide_glyph_1, wide_glyph_2, wide_glyph_3, wide_glyph_4];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max); //.floor is good for arrays since they start at 0!
